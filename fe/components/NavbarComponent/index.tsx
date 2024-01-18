559c9d15d7dc3e5f697511aed63a8eb82dcb69c6
vu minh tu <vuv909@gmail.com>
2024-01-19 03:43:57 +0700

    coding

--- fe/components/NavbarComponent/index.tsx
+++ fe/components/NavbarComponent/index.tsx
@@ -1,30 +1,32 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Logo from "../../public/icons8-logo.svg";
+import Image from "next/image";
+import { Badge } from "primereact/badge";
+import { useRouter } from "next/navigation";
+
 interface NavbarComponentProps {
   colorNavbarOne: string;
   colorNavbarTwo: string;
 }
-
 const NavbarComponent: React.FC<NavbarComponentProps> = ({
   colorNavbarOne,
   colorNavbarTwo,
 }) => {
   const [scrolling, setScrolling] = useState(false);
-
+  const [isLogin, setIsLogin] = useState(false);
+  const router = useRouter();
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY;
       setScrolling(scrollTop > 0);
     };
-
     window.addEventListener("scroll", handleScroll);
-
     // Cleanup the event listener on component unmount
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-
   return (
     <div
       style={{
@@ -41,7 +43,9 @@ const NavbarComponent: React.FC<NavbarComponentProps> = ({
       }}
       className={`${scrolling ? colorNavbarOne : colorNavbarTwo}`}
     >
-      <div style={{ marginLeft: "3vw", zIndex: 100 }}>logo</div>
+      <div style={{ marginLeft: "3vw", zIndex: 100 }} className="cursor-pointer" onClick={() => router.push("/")}>
+        <Image src={Logo} width={50} height={50} alt="logo" />
+      </div>
       <div
         style={{
           display: "flex",
@@ -50,9 +54,31 @@ const NavbarComponent: React.FC<NavbarComponentProps> = ({
           zIndex: 100,
         }}
       >
-        <div>item1</div>
-        <div>item2</div>
-        <div>item3</div>
+        {!isLogin && (
+          <div>
+            <button className="bg-gray-950 hover:bg-gray-800 p-2 text-white rounded-lg" onClick={() => router.push("/login")}>
+              Đăng nhập
+            </button>
+          </div>
+        )}
+        {isLogin && (
+          <div className="flex gap-14 items-center justify-center">
+            <div className="cursor-pointer">
+              <i
+                className="pi pi-bell p-overlay-badge"
+                style={{ fontSize: "1.5rem" }}
+              >
+                <Badge value="2"></Badge>
+              </i>
+            </div>
+            <div className="cursor-pointer">
+              <img
+                src="https://picsum.photos/200/300"
+                className="h-50px w-50px rounded-full"
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
