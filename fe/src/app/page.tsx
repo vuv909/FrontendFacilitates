2ba3bc9423fc7aa60c44a41f403eee9536867343
vu minh tu <vuv909@gmail.com>
2024-01-19 07:41:39 +0700

    coding

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useState } from "react";
 import CarouselComponent from "../../components/CarouselComponent";
 import NavbarComponent from "../../components/NavbarComponent";
@@ -7,8 +8,10 @@ import { Tooltip } from "primereact/tooltip";
 import { InputText } from "primereact/inputtext";
 import CarouselTopComponent from "../../components/CarouselTopComponent";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter()
   const colorNavbarOne: string = "bg-gray-300";
   const colorNavbarTwo: string = "bg-transparent";
   return (
@@ -32,20 +35,21 @@ export default function Home() {
             tooltip="Hello world"
             tooltipOptions={{ position: "top" }}
           />
-          <Link href={'/search'}>
-            <button className="bg-blue-500 hover:bg-blue-300 p-3 rounded-r-lg">
+            <button onClick={()=>router.push('/search')} className="bg-blue-500 hover:bg-blue-300 p-3 rounded-r-lg">
               <span className="pi pi-search text-white h-full"></span>
             </button>
-          </Link>
         </div>
       </div>
 
       <div>
+        <div className="text-center mt-12 font-bold text-2xl">
+          <h1>Phân loại dịch vụ đặt trước</h1>
+        </div>
         <CarouselComponent />
       </div>
 
-      <div className="mt-10">
-        <h1 className="ml-7 text-lg">Top sân bóng đá được sử dụng nhiều</h1>
+      <div className="mt-10 text-center">
+        <h1 className="ml-7 font-bold text-lg">Top các phòng , sân thể dục được sử dụng nhiều</h1>
         <CarouselTopComponent />
       </div>
 
