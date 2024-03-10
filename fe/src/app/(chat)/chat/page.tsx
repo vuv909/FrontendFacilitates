94623bd45f8572aa01f9b142886994f35cb33bfc
trungnq <trungnqhe161514@fpt.edu.vn>
2024-03-10 21:46:59 +0700

    chat with admin

---
+++ fe/src/app/(chat)/chat/page.tsx
@@ -0,0 +1,167 @@
+// components/Chat.js
+'use client'
+import React, { useEffect, useState } from 'react';
+import { Socket } from 'socket.io-client';
+import io from 'socket.io-client';
+import { chat, getListAdminMessage, getListUser, getListUserMessage } from '../../../../services/chat.api';
+interface Message {
+  text: string;
+  sender: 'left' | 'right';
+}
+
+interface User {
+  email: string,
+  _id: string
+}
+const host = "http://localhost:5152"
+const Chat = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [newMessage, setNewMessage] = useState('');
+  const [selectedUser, setSelectedUser] = useState<string | null>(null);
+  const [socket, setSocket] = useState<Socket>()
+  const [users, setUsers] = useState<User[]>([]);
+  const [user, setUser] = useState<User>();
+
+  const sendMessage = () => {
+    if (newMessage.trim() === '') return;
+    setMessages([...messages, { text: newMessage, sender: 'right' }]);
+    setNewMessage('');
+    chat({ userId: selectedUser, type: "admin", content: newMessage })
+      .then(response => {
+        socket?.emit('privateMessage', { receiver: selectedUser, message: newMessage })
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  };
+
+  const handleUserSelect = async (user: string) => {
+    const existedUser = users.find(cUser => cUser._id === user);
+    if(existedUser === undefined){
+      const response = await getListUser();
+      const data = response.data;
+      if(data.statusCode === 1){
+        const listUser = data.data;
+        setUsers(listUser)
+      }
+    }
+    setMessages([]);
+    setSelectedUser(user);
+    try{
+      const response = await getListAdminMessage(user);
+      const data = response.data;
+  
+      if (data.statusCode === 1) {
+        const newMessages = data.data.map((message: any) => ({
+          text: message.content,
+          sender: message.type === 'user' ? 'left' : 'right'
+        }));
+        setMessages(newMessages);
+      }
+    }catch(error){
+      console.error(error);
+    }
+
+  };
+
+  useEffect(() => {
+    const newSocket = io(host);
+    setSocket(newSocket);
+    getListUser()
+      .then(response => {
+        const data = response.data;
+        if (data.statusCode === 1) {
+          const listUser = data.data;
+          setUsers(listUser)
+          setSelectedUser(users[0]._id);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      })
+    socket?.on('connect', () => {
+      console.log('Connected to the server');
+    });
+    const storedData = localStorage.getItem("user");
+    if (storedData) {
+      try {
+        const user = JSON.parse(storedData);
+        newSocket.emit('storeAdminId', user._id);
+        setUser(user);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    return () => {
+      newSocket.disconnect();
+    }
+  }, [])
+
+  useEffect(() => {
+    if (socket) {
+      socket.on('privateMessage', data => {
+        if (data) {
+          const newMessage: Message = {
+            text: data.message,
+            sender: 'left'
+          }
+          setMessages(prevMessage => [...prevMessage, newMessage]);
+          handleUserSelect(data.sender);
+        }
+      })
+    }
+  }, [socket])
+
+  const handleKeyDown = (e: any) => {
+    if (e.key === 'Enter') {
+      sendMessage();
+    }
+  };
+
+  return (
+    <div className="flex h-screen">
+      <div className="w-1/4 bg-gray-200 p-4">
+        <h2 className="text-lg font-semibold mb-4">Danh sách người dùng</h2>
+        <ul>
+          {users.map((user, index) => (
+            <li key={index} className={`cursor-pointer ${selectedUser === user._id ? 'font-bold' : ''}`} onClick={() => handleUserSelect(user._id)}>
+              {user.email}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex-grow bg-gray-100">
+        {/* Chat interface */}
+        <div className="h-screen flex flex-col justify-between bg-gray-100">
+          <div className="overflow-y-auto px-4 pt-4">
+            {messages.map((message, index) => (
+              <div key={index} className={`flex justify-${message.sender === 'right' ? 'end' : 'start'} mb-4`}>
+                <div className={`bg-${message.sender === 'right' ? 'blue' : 'gray'}-500 text-white rounded-lg py-2 px-4`}>
+                  {message.text}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex items-center bg-white border-t border-gray-200 p-4">
+            <input
+              type="text"
+              className="flex-grow border rounded-lg py-2 px-4 mr-2"
+              placeholder="Type your message..."
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+              onKeyDown={handleKeyDown}
+            />
+            <button
+              className="bg-blue-500 text-white py-2 px-4 rounded-lg"
+              onClick={sendMessage}
+            >
+              Gửi
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -11,6 +11,20 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { getFacilities, getFacilityDetail } from "../../services/facilities.api";
 import { getCategory } from "../../services/category.api";
+import { Socket } from 'socket.io-client';
+import io from 'socket.io-client';
+import { changeConfirmLocale } from "antd/es/modal/locale";
+import { chat, getListUserMessage } from "../../services/chat.api";
+interface Message {
+  text: string;
+  sender: 'left' | 'right';
+}
+
+interface User {
+  email: string;
+  _id: string;
+}
+const host = "http://localhost:5152"
 
 export default function Home() {
   const router = useRouter();
@@ -19,27 +33,120 @@ export default function Home() {
   const [faci, setFaci] = useState([]);
   const [cate, setCate] = useState([]);
 
+  // Bắt đầu chat to admin - TrungNQ
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [inputText, setInputText] = useState<string>('');
+  const [showChat, setShowChat] = useState(false);
+  const [socket, setSocket] = useState<Socket>();
+  const [user, setUser] = useState<User>();
+
   useEffect(() => {
     getCategory()
-    .then((response) =>  {
-      setCate(response.data.item)
-    })
-    .catch((error) => console.error("Error fectching Category"))
-    
+      .then((response) => {
+        setCate(response.data.item)
+      })
+      .catch((error) => console.error("Error fectching Category"))
+
 
     getFacilities()
-      .then((response) =>{
+      .then((response) => {
         setFaci(response.data.items)
       })
       .catch((error) => console.error("Error fetching Facilities"))
 
-      // console.log(cate);
-      // console.log(faci);
-      
+
+    getListUserMessage()
+      .then(response => {
+        const data = response.data;
+        if(data.statusCode === 1){
+          console.log(data.data, "hi");
+          
+          const newMessages = data.data.map((message: any) => ({
+            text: message.content,
+            sender: message.type === 'user' ? 'left' : 'right'
+          }));
+          setMessages(newMessages);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      })
+    // console.log(cate);
+    // console.log(faci);
+
   }, [])
 
-  if(!faci || !cate){
-    return(
+  useEffect(() => {
+    const newSocket = io(host);
+    setSocket(newSocket);
+
+    socket?.on('connect', () => {
+      console.log('Connected to the server');
+    });
+    const storedData = localStorage.getItem("user");
+    if (storedData) {
+      try {
+        const user = JSON.parse(storedData);
+        newSocket.emit('storeUserId', user._id);
+        setUser(user);
+        setShowChat(true);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    return () => {
+      newSocket.disconnect();
+    }
+  }, [])
+
+  useEffect(() => {
+    if (socket) {
+      socket.on('privateMessage', data => {
+        if (data) {
+          const newMessage: Message = {
+            text: data.message,
+            sender: 'left'
+          }
+          setMessages(prevMessages => [
+            ...prevMessages, newMessage
+          ]);
+        }
+      })
+    }
+  }, [socket])
+
+  const handleSendMessage = () => {
+    if (inputText.trim() === '') return;
+
+    setMessages([
+      ...messages,
+      {
+        text: inputText.trim(),
+        sender: 'right'
+      }
+    ]);
+    setInputText('');
+    if (user) {
+      chat({ userId: user?._id, type: "user", content: inputText })
+        .then(response => {
+          socket?.emit('privateMessage', { sender: user?._id, message: inputText })
+        })
+        .catch(error => {
+          console.error("Error when chat: ", error);
+        })
+    }
+  }
+
+  const handleKeyDown = (e: any) => {
+    if (e.key === 'Enter') {
+      handleSendMessage();
+    }
+  };
+  // Kết thúc chat to admin- TrungNQ
+
+
+  if (!faci || !cate) {
+    return (
       <div>loading...</div>
     )
   }
@@ -78,15 +185,53 @@ export default function Home() {
         <div className="text-center mt-12 font-bold text-2xl">
           <h1>Phân loại dịch vụ đặt trước</h1>
         </div>
-        <CarouselComponent data={cate}/>
+        <CarouselComponent data={cate} />
       </div>
 
       <div className="mt-10 text-center">
         <h1 className="ml-7 font-bold text-lg">
           Top các phòng , sân thể dục được sử dụng nhiều
         </h1>
-        <CarouselTopComponent data={faci}/>
+        <CarouselTopComponent data={faci} />
       </div>
+
+      <div className="fixed bottom-5 right-5 bg-white p-4 border border-gray-300 overflow-y-auto" style={{ maxHeight: '400px' }}>
+  <h2 className="text-lg font-semibold mb-2">Chat Box</h2>
+  <div className="flex flex-col space-y-2">
+    {messages.map((message, index) => (
+      <div
+        key={index}
+        className={`text-sm p-2 rounded-lg ${message.sender === 'right' ? 'bg-blue-100 self-end' : 'bg-gray-100 self-start'
+          }`}
+      >
+        {message.text}
+      </div>
+    ))}
+  </div>
+  {showChat
+    ?
+    <div className="flex space-x-2 mt-4">
+      <input
+        type="text"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        onKeyDown={handleKeyDown}
+        className="flex-grow p-2 border border-gray-300 rounded-lg"
+        placeholder="Type your message..."
+      />
+      <button
+        onClick={handleSendMessage}
+        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+      >
+        Send
+      </button>
+    </div>
+    : <span>Đăng nhập để chat</span>
+  }
+
+</div>
+
+
 
       <FooterComponent />
     </div>
