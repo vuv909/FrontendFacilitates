8f0e54fc40b4f1172196d93c4f44bd916661c6e9
QuanBui <quanbui030@gmail.com>
2024-03-11 21:41:38 +0700

    services file

---
+++ fe/services/user.api.ts
@@ -0,0 +1,28 @@
+import restClient from "./restClient";
+
+export function getProfile(id:String){
+    return restClient({
+        url:`users/${id}`
+    })
+}
+export function updateProfile(id:String,data:any){
+return restClient({
+    url:`users/${id}`,
+    method:`PUT`,
+    data:data,
+
+})
+}
+
+export function getAllUser(email?:any,page?: number,size?:number){
+    return restClient({
+        url:`users/`,
+        method:`GET`,
+        params: {
+            email:email,
+            page:page,
+            size:6
+        }
+    })
+}
+
